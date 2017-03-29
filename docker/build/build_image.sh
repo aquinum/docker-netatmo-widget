72b53d047a51fd627d1685b31cf6eed4a523ca04
Arnaud de Mouhy <arnaud.demouhy@akerbis.com>
2017-03-29 10:21:24 +0200

    timezone and php intl

--- docker/build/build_image.sh
+++ docker/build/build_image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apk add --update nginx php5-fpm php5-json php5-gettext php5-curl supervisor
+apk add --update nginx php5-fpm php5-json php5-gettext php5-curl php5-intl supervisor tzdata
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 
 adduser -D netatmo
 
